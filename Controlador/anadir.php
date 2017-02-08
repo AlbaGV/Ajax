2a47a7895059c67f4d547964efccd587bd5a8f64
Alba GV <ciuxgokudo@hotmail.es>
2017-02-08 10:32:47 +0100

    Update anadir.php

--- Controlador/anadir.php
+++ Controlador/anadir.php
@@ -4,8 +4,7 @@
 
 error_reporting(E_ALL ^ E_NOTICE);//Que me notifique de todos los errores menso de la notice
 require_once '../Modelo/Contenido.php';
-//  $img = $_FILES["imagen"]["name"];
-// move_uploaded_file($_FILES["imagen"]["tmp_name"]["size"] > 500000, "interfaz_usuario/imagen/" . $img);
+
 
 $perro_Anadir = new Contenido("",$_POST[fechaAltaNuevo],$_POST[nombreNuevo],$_POST[duenoNuevo],$_POST[razaNuevo]);
 
