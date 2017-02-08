ed6fe8802b1d11a22a4d947daa16b8237287edf0
Alba GV <ciuxgokudo@hotmail.es>
2017-02-08 10:33:50 +0100

    Update formulario_nuevos.php

--- Vista/formulario_nuevos.php
+++ Vista/formulario_nuevos.php
@@ -7,7 +7,7 @@
       <div id="th">Nombre del dueño</div><div id="td"> <input type="text" name="dueno" size="10" id="duenoNuevo" required><br></div>
      <div id="th">Raza<select placeholder="Raza" name="idraza" id="idrazanuevo" >
           <?php
-          //Esta es la imagen --><td class="imagen"> $casa->getImagen()</td> <th>Imagen</th>
+       
           foreach ($data['listadoRaza'] as $fila2) {
             ?>
             <option value="<?php echo $fila2->getIdRaza(); ?>"><?php echo$fila2->getNombreRaza(); ?></option>
